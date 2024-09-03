a2034a9dc3f190cb8e9463478f9e2eb1c9925433
GALOOOOO <joca244netto@gmail.com>
2024-09-03 15:05:46 -0300

    Trabalhando com comando de entrada e saída em python

---
+++ exemplo2.py
@@ -0,0 +1,13 @@
+nome = "Victão"
+end = "Chácara Itapiracó"
+idade = 17
+
+#print(nome,end,idade)
+#1ª forma de concatenção
+print("Olá ",nome," seu endereço é ",end," e sua idade é ",idade)
+
+#2ª forma de concatenção
+print("Seja bem vindo {} sua residência é {} você possui {} anos".format(nome,end,idade))
+
+#3ª forma de concatenação
+print(f"Olá,seja bem vindo {nome}, o seu endereço é {end} e sua idade é {idade}")
