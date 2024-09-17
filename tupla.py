2966fe5a1b7c839b9410247830e4377019d58072
GALOOOOO <joca244netto@gmail.com>
2024-09-17 14:07:09 -0300

    Trabalhando com tupla em python

---
+++ tupla.py
@@ -0,0 +1,16 @@
+objetos = ('Lápis','Borracha','Caderno')
+print(objetos[1])
+
+print(type(objetos))
+
+print(objetos)
+
+print('<=>'*20)
+for item in range(3):
+  print(objetos[item], end=', ')
+
+print()
+print('<=>'*20)
+for elementos  in objetos:
+  print(elementos)
+
